41c607c18c83576e5059c95e20b59b1a6f96eac1
zalcandil <eamercado@outlook.com>
2020-06-30 22:49:35 +0200

    editing directly in the  console

--- get-quote.py
+++ get-quote.py
@@ -1,5 +1,6 @@
 import random
 #random is a module from Python that allow me to select a random item inside an array 
+#remember to not import modules inside of a function 
 def Itiswhatitis():
   print("Keep it logically awesome.")
 
@@ -12,7 +13,8 @@ def Itiswhatitis():
   last = len(quotes) - 1
   #That allow me to update automatically the RND the number of variables inside my array.
   rnd = random.randint(0, last)
-  print(quotes[rnd])
+  
+  print(quotes[18])
   #When you want to look for a specific items inside an array, use the [] modifier to tell Python that you are looking for an specific item inside the array.  
   #Remember that arrays always start at 0, so take it into account when you are lloking for an specific line
 if __name__== "__main__":
